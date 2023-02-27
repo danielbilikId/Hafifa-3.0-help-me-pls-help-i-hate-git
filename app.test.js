6d13dec8e5342db85c50a2c595cb737a06e61cf8
igor bilik <danielbilik2003@gmail.com>
2023-02-27 14:00:05 +0200

    Added eslint and modified name clause conventions

--- app.test.js
+++ app.test.js
@@ -1,8 +1,8 @@
 import request from "supertest";
 import  app from "./app";
 
-describe('Test Healthy connection', () => {
-    it('Test health:', () => {
-        request(app).get('/health').expect(200);
+describe('IsSoldierAlive', () => {
+    it('SendAPIGetRequestIfSoldierIsAliveExpected200:', () => {
+        request(app).get('/health').expect("I am alive!");
     });
 });
