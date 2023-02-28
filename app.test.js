dace802cab60908f9e85e5b20620c3e531038afb
daniel_bilik <106474043+danielbilikId@users.noreply.github.com>
2023-02-28 15:05:29 +0200

    Delete app.test.js

--- app.test.js
+++
@@ -1,8 +0,0 @@
-import request from "supertest";
-import  app from "./app";
-
-describe('IsSoldierAlive', () => {
-    it('SendAPIGetRequestIfSoldierIsAliveExpected200:', () => {
-        request(app).get('/health').expect("I am alive!");
-    });
-});
