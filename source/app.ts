099bde4c7d90efad08a6e623302ecfe2fd555245
igor bilik <danielbilik2003@gmail.com>
2023-03-01 10:50:36 +0200

    source

---
+++ source/app.ts
@@ -0,0 +1,12 @@
+import express, { Express, Request, Response } from 'express';
+import helmet from 'helmet';
+
+const app = express();
+
+app.use(helmet());
+
+app.get('/health', (req:Request, res:Response) => {
+  res.send("I am alive!");
+});
+
+export default app;
