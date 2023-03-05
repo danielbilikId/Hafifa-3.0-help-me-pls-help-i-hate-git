683a01e5209896cb524b1307806cd2cf30335e04
igor bilik <danielbilik2003@gmail.com>
2023-03-05 18:03:04 +0200

    readme
    lines

--- source/app.ts
+++ source/app.ts
@@ -1,10 +1,9 @@
 import express, { Express, Request, Response } from 'express';
 import helmet from 'helmet';
-
+import mongoose, { Collection } from 'mongoose';
 const app = express();
 app.use(helmet());
 app.get('/health', (req:Request, res:Response) => {
   res.send("I am alive!");
 });
-
-export default app; 
+export default app;
