danielbilikId/Hafifa_Daniel_Call_of_Duty

testing for all cases
changes 1

added tests

fixup

src/routes/routes.ts
@@ -1,0 +1,20 @@
+import express from 'express';
+import helmet from 'helmet';
+import bodyParser from 'body-parser';
+import { getSoldierByID, getSoldierByQuery, insertSoldier } from '../controller/SoldierController';
+import checkHealth from '../controller/HealthController';
+
+const router = express();
+router.use(helmet());
+
+router.use(bodyParser());
+
+router.get('/health', checkHealth);
+
+router.post('/soldiers', insertSoldier);
+
+router.get('/soldiers/:id', getSoldierByID);
+
+router.get('/soldiers', getSoldierByQuery);
+
+export default router;

danielbilikId/Hafifa-3.0-help-me-pls-help-i-hate-git

src/routes/routes.ts
@@ -1,0 +1,20 @@
+import express from 'express';
+import helmet from 'helmet';
+import bodyParser from 'body-parser';
+import { insertSoldier, getSoldierByID, findSoldierByQuery } from '../controller/SoldierController';
+import checkHealth from '../controller/HealthController';
+
+const router = express();
+router.use(helmet());
+
+router.use(bodyParser());
+
+router.get('/health', checkHealth);
+
+router.post('/soldiers', insertSoldier);
+
+router.get('/soldiers/:id', getSoldierByID);
+
+router.get('/soldiers', findSoldierByQuery);
+
+export default router;